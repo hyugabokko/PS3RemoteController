2f0a381e95b23486d367f0559d9fd6a79817b418
hhyuga <hhyuga201515@gmail.com>
2016-08-20 03:10:52 +0900

    Controllerクラスの追加
    リモコンのスイッチやスティックの使用が簡略化できるクラスの追加
    update関数によって、メンバ変数のセンサの値を更新する

--- PS3RemoteController.h
+++ PS3RemoteController.h
@@ -1,6 +1,4 @@
 
-#pragma once
-
 #include "Arduino.h"
 
 #include "PS3USB.h"
@@ -18,8 +16,25 @@ namespace PS3RemoteController
 	PS3USB PS3_Usb( &Usb );
 	BTD Btd( &Usb );
 	PS3BT PS3_Bt( &Btd );
-    
-    typedef ButtonEnum Button; 
+
+    typedef ButtonEnum Button;
+
+    class Controller {
+        private:
+
+        public:
+	        // structs
+            struct Stick { uint8_t x, y; };
+            struct Sticks { Stick L, R; };
+
+            // var
+            Sticks sticks;
+            void update()
+            {
+    	        sticks.L.x = PS3_Bt.getAnalogHat( LeftHatX );
+	            sticks.L.x = PS3_Bt.getAnalogHat( LeftHatY );
+            }
+    };
 }
 
 #define usb PS3RemoteController::Usb
@@ -30,6 +45,6 @@ namespace PS3RemoteController
 #define controller PS3RemoteController::PS3_Bt
 #endif
 
-#define Button PS3RemoteController::Button
+//#define Button PS3RemoteController::Button
 
 //#include "PS3RemoteController.cpp"
